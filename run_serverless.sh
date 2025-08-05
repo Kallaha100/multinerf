1917f9a847dddc2288df0024a329f8d7d053f11c
Ordis <31918073+Kallaha100@users.noreply.github.com>
2025-08-05 13:41:42 +0200

    Update run_serverless.sh

--- run_serverless.sh
+++ run_serverless.sh
@@ -1,26 +1,18 @@
 #!/bin/bash
 set -e
-
+set -x
 # 1. Prepare directories
 mkdir -p model/checkpoints
-
 #2 get to model
 cd model
-
 # 3. Download/clone your data
 gdown --folder https://drive.google.com/drive/folders/1WHA3v7_HuXpLCjSxCl0MW-z6S4-GkXxs?usp=sharing
-
 #4
 cd ..
-
 # 5. Run training
 python -m train \
   --gin_configs=configs/360.gin \
   --gin_bindings="Config.data_dir = 'model'" \
   --gin_bindings="Config.checkpoint_dir = 'model/checkpoints'" \
   --logtostderr
-
-
 # just specify model/checkpoints as the result to retrieve
-
-
