489a6db880ef1eac4fb35303361160bfd95f6484
Ordis <31918073+Kallaha100@users.noreply.github.com>
2025-08-05 16:29:00 +0200

    Update run_serverless.sh

--- run_serverless.sh
+++ run_serverless.sh
@@ -6,7 +6,7 @@ mkdir -p model/checkpoints
 #2 get to model
 cd model
 # 3. Download/clone your data
-gdown "https://drive.google.com/uc?id=1wBh3w32F7aV5bWWXUd6eMBaLB5MCNJyd"
+gdown "https://drive.google.com/file/d/1C1NT2T9TYgKBJulOCo_gVLhbTs73SB8U"
 unzip nerf.zip
 rm nerf.zip
 #4
@@ -18,4 +18,5 @@ python -m train \
   --gin_bindings="Config.checkpoint_dir = 'model/checkpoints'" \
   --logtostderr
 # just specify model/checkpoints as the result to retrieve
+
 
