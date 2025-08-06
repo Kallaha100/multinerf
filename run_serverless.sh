803ee53772c6a3079f0f9ee1728c250f4e43d6cf
Ordis <31918073+Kallaha100@users.noreply.github.com>
2025-08-06 07:43:43 +0200

    Update run_serverless.sh

--- run_serverless.sh
+++ run_serverless.sh
@@ -6,7 +6,7 @@ mkdir -p model/checkpoints
 #2 get to model
 cd model
 # 3. Download/clone your data
-gdown --id 1C1NT2T9TYgKBJulOCo_gVLhbTs73SB8U -O nerf.zip
+gdown --id 1lWld8MCUf4tGdq6BBiskzeAglcXOai0T -O nerf.zip
 unzip nerf.zip
 rm nerf.zip
 #4
@@ -16,8 +16,10 @@ python -m train \
   --gin_configs=configs/360.gin \
   --gin_bindings="Config.data_dir = 'model'" \
   --gin_bindings="Config.checkpoint_dir = 'model/checkpoints'" \
+  --gin_bindings="Config.max_steps = 5000" \
   --logtostderr
 # just specify model/checkpoints as the result to retrieve
+
 
 
 
